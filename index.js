c96f516c75a774dba4c92691b608fc4650f36f3f
Luis Ramirez <luis21x@outlook.com>
2021-11-03 00:26:46 -0400

    genreating html

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
 const Inquirer = require('inquirer');
 const Jest = require('jest');
-const Employee = require('./employee');
+const Employee = require('./lib/employee');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 const inquirer = require('inquirer');
+const fs = require('fs');
 
-const htmlStartString = '';
-const htmlBodyString ='';
-const htmlEndString = '';
+const htmlStart = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile | Home </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./dist/style.css">
+</head>
+<body>    
+<div class='header'>
+<h1>My Team</h1>
+</div>`;
+const htmlEnd = `</body>
+</html>`;
+let htmlBodyString ='';
 let employeeList = [];
 
 const managerQuestionPrompt = [
     {
         type: 'input',
         message: "What is the team manager's name?",
-        name: 'managerName',
+        name: 'name',
     },
     {
         type: "input",
         message:"What is the team manager's id?",
-        name: "managerId",
+        name: "id",
     },
     {
         type: "input",
         message:"What is the team manager's email?",
-        name: "managerEmail",
+        name: "email",
     },
     {
         type: "input",
         message:"What is the team manager's office number?",
-        name: "managerOfficeNumber",
+        name: "officeNumber",
     },
 
 ];
@@ -39,17 +54,17 @@ const engineerQuestionPrompt = [
     {
         type: 'input',
         message: "What is your engineer's name?",
-        name: 'engineerName',
+        name: 'name',
     },
     {
         type: 'input',
         message: "What is your engineer's id?",
-        name: 'engineerId',
+        name: 'id',
     },
     {
         type: 'input',
         message: "What is your engineer's email?",
-        name: 'engineerEmail',
+        name: 'email',
     },
     {
         type: 'input',
@@ -62,26 +77,25 @@ const internQuestionsPrompt =[
     {
         type: 'input',
         message: "What is your intern's name?",
-        name: 'internName',
+        name: 'name',
     },
     {
         type: 'input',
         message: "What is your intern's id?",
-        name: 'internId',
+        name: 'id',
     },
     {
         type: 'input',
         message: "What is your intern's email?",
-        name: 'internEmail',
+        name: 'email',
     },
     {
         type: 'input',
         message: "What is your intern's school?",
-        name: 'internSchool',
+        name: 'school',
     },
 
 ]
-
 function anotherEmployeePrompt(){
     inquirer
         .prompt(    {
@@ -94,53 +108,50 @@ function anotherEmployeePrompt(){
             if(response.employeeType === 'Engineer'){
                 inquirer.prompt(engineerQuestionPrompt)
                         .then((response) =>{
-                            const engineer = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.engineerGithub);
+                            const engineer = new Engineer(response.name, response.id, response.email, response.engineerGithub);
                             employeeList.push(engineer);
                             anotherEmployeePrompt();
                         })
             } else if(response.employeeType === 'Intern'){
                 inquirer.prompt(internQuestionsPrompt)
                         .then((response) =>{
-                            const intern = new Intern(response.internName, response.internId, response.interEmail, response.internSchool);
+                            const intern = new Intern(response.name, response.id, response.email, response.internSchool);
                             employeeList.push(intern);
                             anotherEmployeePrompt();
                         })
-            } else{
-                break;
+            } else if (response.employeeType === "I don't want to add any more team members"){
+              createEmployeeCards();
+              fs.writeFile('myTeam.html', createEmployeeCards(),(error, data) =>
+              error ? console.error(error) : console.log('succesfully created'))
             }
         })
 }
 
+inquirer.prompt(managerQuestionPrompt).then((employee) =>{
+    const manager = new Manager(employee.name, employee.id, employee.email, employee.officeNumber)
+    employeeList.push(manager);
+    anotherEmployeePrompt();
+    
+})
+
 function createEmployeeCards(){
     employeeList.forEach(employee =>{
          // add boot strap html here
-         htmlBodyString = htmlBodyString + `
+         htmlBodyString += `
          <div class="card text-white bg-primary mb-3" style="width: 18rem;">
          <div class="card-body">
-           <h5 class="card-title">Card title</h5>
-           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+           <h5 class="card-title">${employee.getName()}</h5>
+           <h5 class="card-title">${employee.getRole()}</h5>
          </div>
          <div class="employee-info">
          <ul class="list-group list-group-flush">
-           <li class="list-group-item">Cras justo odio</li>
-           <li class="list-group-item">Dapibus ac facilisis in</li>
-           <li class="list-group-item">Vestibulum at eros</li>
+           <li class="list-group-item">${employee.getId()}</li>
+           <li class="list-group-item">${employee.getEmail()}</li>
+           <li class="list-group-item">${employee.getUnique()}</li>
          </ul>
          </div>
        </div>`
     })
+    return htmlStart + htmlBodyString + htmlEnd;
 }
-
-    inquirer
-    .prompt(
-        managerQuestionPrompt
-        )
-        .then((response) => {
-            const manager = new Manager(response.managerName, response.managerId, response.managerEmail, response.managerOfficeNumber)
-            employeeList.push(manager);
-            anotherEmployeePrompt();
-        })
-        .then()
-
-console.log('Please build your team')
 
