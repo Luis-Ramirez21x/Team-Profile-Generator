85123e30f0ffbe44fa42593d9cc507a502bc2511
Luis Ramirez <luis21x@outlook.com>
2021-11-02 22:35:43 -0400

    adding bootstrap cards and cxreating list of arrayed objects

--- index.js
+++ index.js
@@ -111,6 +111,26 @@ function anotherEmployeePrompt(){
         })
 }
 
+function createEmployeeCards(){
+    employeeList.forEach(employee =>{
+         // add boot strap html here
+         htmlBodyString = htmlBodyString + `
+         <div class="card text-white bg-primary mb-3" style="width: 18rem;">
+         <div class="card-body">
+           <h5 class="card-title">Card title</h5>
+           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+         </div>
+         <div class="employee-info">
+         <ul class="list-group list-group-flush">
+           <li class="list-group-item">Cras justo odio</li>
+           <li class="list-group-item">Dapibus ac facilisis in</li>
+           <li class="list-group-item">Vestibulum at eros</li>
+         </ul>
+         </div>
+       </div>`
+    })
+}
+
     inquirer
     .prompt(
         managerQuestionPrompt
